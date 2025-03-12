1ebfc3dffd0abcb0a141050751887073680c8579
k-risu <kkd4869@gmail.com>
2025-03-12 12:57:52 +0900

    Data 처리 및 Page 처리

--- src/app/(with-search)/search/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div style={{ border: "5px solid green" }}>
-      <h1>여기는 검색 레이아웃</h1>
-      <div>{children}</div>
-    </div>
-  );
-}
