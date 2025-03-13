aaf4264bd7d70659209bb4181c3bb97d3d505ce7
k-risu <kkd4869@gmail.com>
2025-03-13 11:53:04 +0900

    loading

--- src/app/(with-search)/search/page.tsx
+++ src/app/(with-search)/search/page.tsx
@@ -1,18 +1,10 @@
 import style from "@/app/(with-search)/search/page.module.css";
 import GoodItem from "@/components/good-item";
-
+import GoodItemSkeleton from "@/components/skeleton/good-item-skeleton";
 import { GoodDataType } from "@/types/types";
-// 쿼리 처리하기
-// 아래 페이지는 쿼리를 서버에서 읽어들여서 처리함.
-// http://localhost:3000/search?keword=iu
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ keyword: string }>;
-}) {
-  const { keyword } = await searchParams;
-  // console.log(keyword);
-  // 제품 카테고리 검색으로 진행하겠습니다.
+import { Suspense } from "react";
+
+async function SearchResult({ keyword }: { keyword: string }) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/products/category/${keyword}`
   );
@@ -20,17 +12,30 @@ export default async function Page({
   if (goods.length === 0) {
     return <div>{keyword} 카테고리에 해당하는 제품이 없습니다.</div>;
   }
+  return (
+    <div>
+      {goods.map((good) => (
+        <GoodItem key={good.id} {...good} />
+      ))}
+    </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ keyword: string }>;
+}) {
+  const { keyword } = await searchParams;
 
   return (
     <div className={style.container}>
       <h4>
         카테고리명 : <strong>{keyword}</strong> 검색페이지
       </h4>
-      <div>
-        {goods.map((good) => (
-          <GoodItem key={good.id} {...good} />
-        ))}
-      </div>
+      <Suspense fallback={<GoodItemSkeleton />}>
+        <SearchResult keyword={keyword} />
+      </Suspense>
     </div>
   );
 }
