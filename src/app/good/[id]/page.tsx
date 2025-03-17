de275f0436abda224ff67a0bafc65ec8fc6b82be
k-risu <kkd4869@gmail.com>
2025-03-17 11:31:03 +0900

    Server Action

--- src/app/good/[id]/page.tsx
+++ src/app/good/[id]/page.tsx
@@ -1,27 +1,29 @@
-import { GoodDataType } from "@/types/types";
 import style from "@/app/good/[id]/page.module.css";
+import CateList from "@/components/cate-list";
+import Editor from "@/components/editor";
+import { GoodDataType } from "@/types/types";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 // 특정한 페이지를 Static Page 로 생성
 export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }, { id: "4" }];
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  // console.log(id);
-
+// 상세화면 컴포넌트
+async function Detail({ id }: { id: string }) {
   let good: GoodDataType | null = null;
-
   try {
+    // const res = await fetch(
+    //   `${process.env.NEXT_PUBLIC_API_URL}/products/${id}`,
+    //   {
+    //     cache: "force-cache",
+    //   }
+    // );
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/products/${id}`,
       {
-        cache: "force-cache",
+        next: { tags: [`good-${id}`] },
       }
     );
     good = await res.json();
@@ -31,11 +33,12 @@ export default async function Page({
   }
 
   if (!good) {
-    return <div>존재하지 않는 상품입니다.</div>;
+    // 404 띄우기
+    notFound();
+    // return <div>존재하지 않는 상품입니다.</div>;
   }
 
   const { title, image, category, rating, description } = good;
-
   return (
     <div className={style.container}>
       <div className={style.title}>{title}</div>
@@ -47,6 +50,25 @@ export default async function Page({
         Rating: {rating.rate} | {rating.count}
       </div>
       <div className={style.description}>{description}</div>
+    </div>
+  );
+}
+
+// 사용자 평가 입력 컴포넌트
+// 서버액션 처리
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  // console.log(id);
+  return (
+    <div>
+      <Detail id={id} />
+      <Editor />
+      <CateList id={id} />
     </div>
   );
 }
--- src/components/random-goods.tsx
+++ src/components/random-goods.tsx
@@ -1,20 +1,25 @@
 import { GoodDataType } from "@/types/types";
 import GoodItem from "./good-item";
 
-export default async function RandomGoods() {
+export async function RandomGoods() {
   let randomGoods: GoodDataType[] = [];
   try {
     const resRandom = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/product?limit=3`
+      `${process.env.NEXT_PUBLIC_API_URL}/products?limit=3`,
+      {
+        next: {
+          revalidate: 3,
+        },
+      }
     );
     randomGoods = await resRandom.json();
+    // console.log(randomGoods);
   } catch (error) {
     console.log(error);
   }
   if (randomGoods.length === 0) {
-    return <div>상품이 없습니다</div>;
+    return <div>상품이 없습니다.</div>;
   }
-
   return (
     <>
       {randomGoods.map((good) => (
